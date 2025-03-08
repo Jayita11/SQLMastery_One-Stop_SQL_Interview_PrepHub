1ef300cc2a532d0780447b230f5ac94519e5a999
Jayita <164938788+Jayita11@users.noreply.github.com>
2025-03-07 23:22:55 -0500

    Add files via upload

---
+++ DataLemur/Easy/20_Pharmacy_Analytics_(Part 2)_Solution.sql
@@ -0,0 +1,11 @@
+SELECT
+  manufacturer,
+  COUNT(drug) AS count_drug, 
+  SUM(cogs - total_sales) AS total_loss
+FROM pharmacy_sales
+WHERE cogs > total_sales
+GROUP BY manufacturer
+ORDER BY total_loss DESC;
+
+
+
