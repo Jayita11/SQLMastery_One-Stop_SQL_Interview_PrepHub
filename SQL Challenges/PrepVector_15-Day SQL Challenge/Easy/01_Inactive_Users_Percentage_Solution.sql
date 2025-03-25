a873b09e4cfc5948fb885c1d3ba1506b6c9b1478
Jayita <164938788+Jayita11@users.noreply.github.com>
2025-03-25 17:09:10 -0400

    Add files via upload

--- SQL Challenges/PrepVector_15-Day SQL Challenge/Easy/01_Inactive_Users_Percentage_Solution.sql
+++ SQL Challenges/PrepVector_15-Day SQL Challenge/Easy/01_Inactive_Users_Percentage_Solution.sql
@@ -1,3 +1,13 @@
+#You’re given two tables: users and events. The events table holds values of all of the user events in the action column (‘like’, ‘comment’, or ‘post’).
+#Write a query to get the percentage of users that have never liked or commented, rounded to two decimal places.
+
+#Output Schema:
+#Column       Type
+#percentage   FLOAT
+
+
+
+
 SELECT 
   ROUND(
     COUNT(*) * 100.0 / (SELECT COUNT(*) FROM users), 
